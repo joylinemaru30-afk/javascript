ae1959e1d3d56f13931d02e25e80b2dff0c9ec7a
pi342us-g <ndubipius96@gmail.com>
2025-08-27 12:52:21 +0300

    added lesson1d

---
+++ lesson1d.js
@@ -0,0 +1,20 @@
+// interger whole number
+let age =49
+console .log("Age of the person:",age)
+// floating point a number with a decimal
+let quantity=35.65
+console .log("The quantity of the sugar bought is :",quantity)
+// boolean a value that evalute to either true or false
+let isadult=true
+let isregistered =false
+
+console .log("the person an adult?",isadult)
+console .log("has he been registered?",isregistered)
+
+// undefined this is a data type that is declared wiyhout assinging any values
+let undefined;
+console .log(undefined)
+
+// null it is a data type that contains null values
+let user =null;
+console .log(user)
