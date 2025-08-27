202144481eac1a97052fe785cc5bc41c074fa6e1
pi342us-g <ndubipius96@gmail.com>
2025-08-27 12:34:11 +0300

    added lesson2

---
+++ lesson1b.js
@@ -0,0 +1,6 @@
+let weight = 10.5    //declearing a variable with the initializer'let'
+const pi=3.142  //declairing a variable with the initializer'const'
+
+// show the output
+console .log(weight)
+console.log(pi)
