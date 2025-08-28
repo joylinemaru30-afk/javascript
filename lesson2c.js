027f18470d7821604f91a0cebc2330323008f43e
pi342us-g <ndubipius96@gmail.com>
2025-08-28 12:41:12 +0300

    lesson

---
+++ lesson2c.js
@@ -0,0 +1,12 @@
+// if conditional statement
+// it is used in the control flow execution structure of a program
+// it executes a block of code /statement if and when a condition is met
+
+let age =28;
+if (age <18){
+    console .log ("you are too young to watch this movie.")
+}
+else{
+    console .log("sit down and enjoy the episode.")
+}
+     
---
+++ lesson2d.js
@@ -0,0 +1,22 @@
+// if...else if statement
+// it is used to evaluate multiple conditions
+
+
+
+let age=-2
+if (age<15){
+    console .log ("you are too young to watch this episode")
+
+}
+
+else if(age>15 && age<=18){
+    console.log("you can watch it with parental guidance")
+
+}
+else if(age>=18 && age <40){
+    console .log("have a seat and enjoy the movie")
+    
+}
+else{
+    console .log("this movie does not siute you.")
+}
