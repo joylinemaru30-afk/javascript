aa99c798a1adc919be3b059ef3721dffef3b9e7b
pi342us-g <ndubipius96@gmail.com>
2025-08-28 11:32:41 +0300

    task

---
+++ task1.js
@@ -0,0 +1,11 @@
+ let car={
+    type:"prado",
+    color:"white",
+    cost:"120000"
+    
+
+}
+console.log("the car color is :",car.color)
+
+vehicles=["buses","prado","bmw","benze"]
+console.log ("the cheap car is",vehicles[0])
