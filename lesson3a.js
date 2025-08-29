045139c04fd04acb258d22211d94422bfd27a45c
pi342us-g <ndubipius96@gmail.com>
2025-08-29 11:51:28 +0300

    lesson

---
+++ lesson3a.js
@@ -0,0 +1,52 @@
+// // javascript for loop 
+// // loops are used to iterate through something
+// // structure of the for loop
+// // 1.start value
+// // 2.condition
+// // 3.increment/decrement
+// // 4.body~the statements to be printed out
+
+
+// // for(let i=0;i<=10;i++){
+// //     console.log("the new value of i is:",i)
+
+// // }
+
+
+
+
+
+// // for (let year=2000;year<=2021;year+=4){
+// //     console.log("is a leap year:",year)
+// // }
+
+// for(let year=1999;year<=2021;year++){
+//     if(year%4==0){
+//         console .log(year,"is a leap year")
+
+//     }
+//     else{
+//         console .log(year,"is not a leap year")
+//     }
+
+
+// let count=0
+// while (count<=10){
+//     console.log(count);
+//     count++;
+// }
+
+for (let i=10;i>=0;i--){
+    
+
+
+
+if (i==5){
+    
+    console .log("the number is five")
+}
+
+else{
+    console .log(i)
+}
+}
---
+++ task2.js
@@ -0,0 +1,8 @@
+let num=24
+
+if(num %4==0){
+    console .log("is odd number")
+}
+else{
+    console .log(" is even number")
+}
