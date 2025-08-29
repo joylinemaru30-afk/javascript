4c1c611da87221483c7440f45994f8f4390b6b01
pi342us-g <ndubipius96@gmail.com>
2025-08-29 12:00:09 +0300

    lesson

---
+++ lesson3b.js
@@ -0,0 +1,21 @@
+// while loop
+// it is commonly used when the lenght of items is not known
+// structure
+// 1.initilization
+// while keyboard
+// condition to be checked
+// increment/decrement
+
+let i =0;
+
+while (i<=10){
+    console .log(i)
+    i++
+}
+ 
+
+let count=10;
+while (count>=1){
+    console .log(count)
+    count--
+}
