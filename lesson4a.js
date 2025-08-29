6fdad23aa9f004827c1118984c271c595df2d821
pi342us-g <ndubipius96@gmail.com>
2025-08-29 13:33:44 +0300

    lesson

---
+++ lesson4a.js
@@ -0,0 +1,17 @@
+// javascript functions
+// structure of a function
+//function keyword,name of the function,parenthesis followed by the block of statements to be executed.
+
+
+function sayhello(){
+    console .log("hello there.hope you are fine. nice to meet you...")
+}
+//call/invoke the function by its name
+sayhello();
+
+
+function good(){
+    console .log("good bye nice to have met you..")
+
+}
+good();
---
+++ task3.js
@@ -0,0 +1,11 @@
+let year =1669
+while( year<2021){
+if(year%4==0){
+console.log("leap year",year)
+}
+else{
+    console.log(year)
+
+}
+year++
+}
