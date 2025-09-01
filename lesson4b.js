440b2cd7c8be7f6311266b29bd0036506753feff
pi342us-g <ndubipius96@gmail.com>
2025-09-01 12:14:38 +0300

    lesson

---
+++ lesson4b.js
@@ -0,0 +1,22 @@
+//functions with parameters
+
+function greeting(name){
+    console .log("hello",name,"how have you been")
+};
+greeting("Sierra")
+
+ function countries(name){
+    console.log("my favourite counrty",name)
+ };
+ countries("kenya");
+ countries("uganda");
+ countries("Rwanda");
+
+
+function sum(a,b,c){
+    let total =a + b + c
+
+    console.log("The total of the numbers is",total)
+};
+sum(22,44,88);
+sum(50,70,60);
