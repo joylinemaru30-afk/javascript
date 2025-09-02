3391d74fe6634e8afa93b3bac55fb5502d027752
pi342us-g <ndubipius96@gmail.com>
2025-09-02 11:34:49 +0300

    message

---
+++ lesson5c.js
@@ -0,0 +1,16 @@
+// Arrow functions with parameters
+const greet  =(jina)=>{
+    console .log("hello",jina,"umeshindaje? Natumaini umeshinda vyema. ")
+}
+
+greet("Mary")
+greet("Jael")
+
+// below is an arrow function to calculate the sum of two numbers
+const total= (a,b)=>{
+    let sum=a+b
+    console .log("the total of the two numbers is:",sum)
+}
+
+total(40,50)
+total(70,90)
