b780a584887f9bccee3b966e3efc2007d1b987d3
pi342us-g <ndubipius96@gmail.com>
2025-09-02 10:56:46 +0300

    lesson

---
+++ lesson5a.js
@@ -0,0 +1,13 @@
+//Anonymous functions in javascript
+const greet=function(){
+    console.log("hello there.how are you?")
+}
+
+greet();
+
+// second example
+(function(){
+    console.log("welcome to the world of programing.")
+
+})();
+
