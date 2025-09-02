7585fa9f0c628b4329bce04f965401c084657d52
pi342us-g <ndubipius96@gmail.com>
2025-09-02 11:18:21 +0300

    lesson

---
+++ lesson5b.js
@@ -0,0 +1,17 @@
+// Arrow functions
+const sayhello =() =>{
+    console .log("This is the arrow function")
+};
+
+// invoke the arrow function
+
+sayhello();
+
+
+
+const good=()=>{
+    console .log("goodbye")
+
+};
+
+good();
